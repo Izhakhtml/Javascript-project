7ed1762c022a8a831b496f06be290a278278a925
Izhakhtml <Izhaklijalem28@gmail.com>
2021-09-29 13:57:38 +0300

    change js

--- fruits and vegetables/fruit.js
+++ fruits and vegetables/fruit.js
@@ -1,13 +1,13 @@
 for (let i = 0; i < products.length; i++) {
-  if (products[i].category=="Fruits and Vegetables") {
-/////////////////////////////////   זה כדי להראות את הדרך של הלולאה המקוננת
-      // for (let j = 0; j < products[i].image.length; j++) {
-      // if (products[i].category=="Fruits and Vegetables") {
-      // container.innerHTML += `<img src =${products[i].image[j]}>`
-      // }}
-////////////////////////////////  
-       container.innerHTML += 
-       `<section>
+  if (products[i].category == "Fruits and Vegetables") {
+    /////////////////////////////////   זה כדי להראות את הדרך של הלולאה המקוננת
+    // for (let j = 0; j < products[i].image.length; j++) {
+    // if (products[i].category=="Fruits and Vegetables") {
+    // container.innerHTML += `<img src =${products[i].image[j]}>`
+    // }}
+    ////////////////////////////////  
+    container.innerHTML +=
+      `<section>
          <article class = "milks">
          <img src =${products[i].image[0]}>
          <h1 class ="changeFont">${products[i].name}</h1><br>
@@ -16,17 +16,16 @@ for (let i = 0; i < products.length; i++) {
          <button class = "btn">aad to cart</button>
          </article>
         </section>`
-  }  
-  
+  }
+
 }
 
 let btn = document.getElementsByClassName("btn")
 for (let i = 0; i < btn.length; i++) {
-   let button = btn[i]
-   button.onclick = ()=>{
-      cartProducts.push(products[i])
-      console.log(cartProducts);
-   }
+  let button = btn[i]
+  button.onclick = () => {
+    cartProducts.push(products[i])
+    console.log(cartProducts);
+  }
 }
 
- 
